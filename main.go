3fcd46128306795efbecbe595c3cee450bed542b
tacheraSasi <tacherasasi@gmail.com>
2024-10-31 00:48:18 +0300

    main fixed

--- main.go
+++ main.go
@@ -3,15 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	// Sample input to our language
 	input := "5 + 3 - 2"
 	fmt.Println("Input:", input)
 
-	// Step 1: Lexing
+
 	tokens := Lexer(input)
 	fmt.Println("Tokens:", tokens)
 
-	// Step 2: Parsing and Evaluating
+
 	result := Parse(tokens)
 	fmt.Println("Result:", result)
 }
